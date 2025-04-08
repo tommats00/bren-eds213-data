458d7e2f42d028b510489865645921a7386b2c1e
tommats00 <tbg@bren.ucsb.edu>
2025-04-08 16:40:44 -0700

    working with SQL

---
+++ week1_data_modeling.sql
@@ -0,0 +1,13 @@
+CREATE TABLE Snow_survey (
+    Site TEXT
+    Year INTEGER
+    Date DATE
+    Plot TEXT
+    Location TEXT
+    Snow_cover REAL 
+    Water_cover REAL
+    Land_cover REAL
+    Total_cover REAL
+    Observer TEXT
+    Notes TEXT
+);
