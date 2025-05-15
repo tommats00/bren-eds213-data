a6a49a231186150fb3d7d9e9ac4d488b9b315f42
tommats00 <tbg@bren.ucsb.edu>
2025-05-15 16:44:13 -0700

    finished with homework

--- week7/little_bobby_tables.sql
+++ week7/little_bobby_tables.sql
@@ -5,12 +5,14 @@
 -- SELECT * FROM Students WHERE (name = '%s' AND...);
 
 -- If Bobby tables name is 'Robert'); DROP TABLE Students;--
--- Then the query will end the statement after the first semicolon and continue to the second statement where they drop all tables
+-- Then the query will end the statement after the first semicolon and continue to the second statement where they drop all tables. 
+-- This is because there is a ' that will end the string. 
 -- The two hyphens at the end will comment out the rest of the code.
 
 -- Part 2:
 -- Suppose instead the school system executed the query
--- SELECT * FROM Studentts WHERE name = '%s'
+-- SELECT * FROM Students WHERE name = '%s'
 
 -- What would Bobby Table's name have to be to destroy things?
--- 
+-- I think the name would be very similar. It would still contain the ' to end the string and execute the drop statement. 
+-- Something like Robert'; DROP TABLE Students;--
